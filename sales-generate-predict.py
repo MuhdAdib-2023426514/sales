562ea9176f9686a1b2c5bb263e4b2e2a77017550
MUHAMMAD ADIB BIN MOHAMED <153445680+MuhdAdib-2023426514@users.noreply.github.com>
2023-12-21 14:04:25 +0800

    Add files via upload

---
+++ sales-generate-predict.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import pandas as pd
+import sklearn
+imprt xgboost
+
+st.write("# Aplikasi Ramalan Jualan Pernigaan")
+st.write("Aplikasi ini meramalkan **Jualan** anda!")
+
+st.sidebar.header('Berikan perbelanjaan pengiklanan anda')
+
+def user_input_features():
+    tv = st.sidebar.slider('TV', 0.0, 300.0, 1)
+    radio = st.sidebar.slider('Radio', 0.0, 50.0, 1)
+    newspaper = st.sidebar.slider('Surat Khabar', 0.0, 115.0, 1)
+    data = {'TV': tv,
+            'Radio': radio,
+            'Surat Khabar': newspaper,}
+    features = pd.DataFrame(data, index=[0])
+    return features
+
+df = user_input_features()
+
+st.subheader('Perbelanjaan pengiklanan anda')
+st.write(df)
+
+regressor = RandomForestRegressor(n_estimators=10, random_state=0, oob_score=True)
+predictions = regressor.predict(df)
+
+st.subheader('Ramalan')
+st.write(predictions)
+
